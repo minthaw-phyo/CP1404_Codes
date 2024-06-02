bc73ae3677fa097e258be92403c5b9e9af8910bc
Ugyen-Kinley <ugyenkinley.singye@my.jcu.edu.au>
2024-06-02 22:03:39 +1000

    Create password_stars.py

---
+++ prac_02/password_stars.py
@@ -0,0 +1,19 @@
+def get_password():
+    password = input("Enter your password: ")
+    while len(password) < 8:
+        print("Password too short")
+        password = input("Enter your password: ")
+    return password
+
+
+def print_asterisks(password):
+    print('*' * len(password))
+
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+if __name__ == "__main__":
+    main()
