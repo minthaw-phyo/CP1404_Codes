2b054ce6315d3d1ec7cf260649c884b9e0bfbf6a
minthawphyo <dividechange@gmail.com>
2025-03-07 15:42:00 +0800

    Complete prac6 car,used_cars and programming_language exercises

---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+
+class ProgrammingLanguage:
+    """Represent a programming language with its attributes."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True if the programming language is dynamically typed."""
+        return self.typing.lower() == "dynamic"
+
+    def __str__(self):
+        """Return a string representation of the programming language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
