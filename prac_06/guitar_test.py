aa438220d38699c556ff34074cdc98d3724fd527
minthawphyo <dividechange@gmail.com>
2025-03-11 00:05:55 +0800

    change the datetime in guitar_test.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,6 +1,8 @@
 from prac_06.guitar import Guitar
+import datetime
 
-current_year = 2022  # Use a fixed year to match the sample output
+current_year = datetime.date.today().year
+
 guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
 guitar2 = Guitar("Another Guitar", 2013, 1000.00)
 
